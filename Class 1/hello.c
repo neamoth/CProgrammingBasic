5600d49bba920f0591df9da418630629cd346127
Neamoth <neamothku@gmail.com>
2022-07-31 20:39:42 +0600

    Class-1

---
+++ Class 1/hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    a = 7;
+    a = a + 1;
+    a++; //increment, its increase the value +1
+    printf("The value of a = %d\nThis is a new line", a); // \n Escape Sequence
+    return 0;
+}
+
+
+// Single comment
+
+/*
+    This is a multiline comment
+    we can write anything in this comment block
+*/
